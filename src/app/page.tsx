544379effff43925b026bab347452e198db6e88e
Juan Mora Romero <info@juanmoraromero.com>
2024-11-26 16:40:27 +0100

    build improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
 
 export default function HomePage() {
+
+  const mockUrls = [
+    'https://utfs.io/f/nLOFjVrvhB5HA2FO3tokqvdJnobFpKtf1wi3xaQR5BM4VPGO',
+    'https://utfs.io/f/nLOFjVrvhB5HFus6Pfn7odHcr3Unt5DTzC0kYm9EgQJaBsPx',
+    'https://utfs.io/f/nLOFjVrvhB5H2Fhv7bsVS78Ud0bZisxWrTHpJReuDwFjLcaB'
+  ]
+
+  const mockImages = mockUrls.map((url, index) => ({
+    id: index + 1,
+    url,
+  }))
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (Gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages,...mockImages].map((image) => (
+          <div key={image.id * Math.random()} className="w-48">
+            <img src={image.url}  />
+          </div>
+        ))}
+      </div>
+
     </main>
   );
 }
