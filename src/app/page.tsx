b7131c3bbf52f5c257542a2144d97b1714fcb3a3
Juan Mora Romero <info@juanmoraromero.com>
2024-11-27 10:13:47 +0100

    linked database to UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,29 @@ import Link from "next/link";
 
 export const dynamic = 'force-dynamic'
 
-  const mockUrls = [
-    'https://utfs.io/f/nLOFjVrvhB5HA2FO3tokqvdJnobFpKtf1wi3xaQR5BM4VPGO',
-    'https://utfs.io/f/nLOFjVrvhB5HFus6Pfn7odHcr3Unt5DTzC0kYm9EgQJaBsPx',
-    'https://utfs.io/f/nLOFjVrvhB5H2Fhv7bsVS78Ud0bZisxWrTHpJReuDwFjLcaB'
-  ]
+  // const mockUrls = [
+  //   'https://utfs.io/f/nLOFjVrvhB5HA2FO3tokqvdJnobFpKtf1wi3xaQR5BM4VPGO',
+  //   'https://utfs.io/f/nLOFjVrvhB5HFus6Pfn7odHcr3Unt5DTzC0kYm9EgQJaBsPx',
+  //   'https://utfs.io/f/nLOFjVrvhB5H2Fhv7bsVS78Ud0bZisxWrTHpJReuDwFjLcaB'
+  // ]
 
-  const mockImages = mockUrls.map((url, index) => ({
-    id: index + 1,
-    url,
-  }))
+  // const mockImages = mockUrls.map((url, index) => ({
+  //   id: index + 1,
+  //   url,
+  // }))
 
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findMany()
-  console.log(posts)
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id)
+  })
+  console.log(images)
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages,...mockImages].map((image) => (
+
+        {[...images, ...images,...images].map((image) => (
           <div key={image.id * Math.random()} className="w-48">
             <img src={image.url}  />
           </div>
