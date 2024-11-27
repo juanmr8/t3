acaccf462aac22d75cdaccb85afb898fac63cd64
Juan Mora Romero <info@juanmoraromero.com>
2024-11-27 12:31:49 +0100

    added auth in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,45 @@
 import { db } from "@/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
-  // const mockUrls = [
-  //   'https://utfs.io/f/nLOFjVrvhB5HA2FO3tokqvdJnobFpKtf1wi3xaQR5BM4VPGO',
-  //   'https://utfs.io/f/nLOFjVrvhB5HFus6Pfn7odHcr3Unt5DTzC0kYm9EgQJaBsPx',
-  //   'https://utfs.io/f/nLOFjVrvhB5H2Fhv7bsVS78Ud0bZisxWrTHpJReuDwFjLcaB'
-  // ]
+// const mockUrls = [
+//   'https://utfs.io/f/nLOFjVrvhB5HA2FO3tokqvdJnobFpKtf1wi3xaQR5BM4VPGO',
+//   'https://utfs.io/f/nLOFjVrvhB5HFus6Pfn7odHcr3Unt5DTzC0kYm9EgQJaBsPx',
+//   'https://utfs.io/f/nLOFjVrvhB5H2Fhv7bsVS78Ud0bZisxWrTHpJReuDwFjLcaB'
+// ]
 
-  // const mockImages = mockUrls.map((url, index) => ({
-  //   id: index + 1,
-  //   url,
-  // }))
+// const mockImages = mockUrls.map((url, index) => ({
+//   id: index + 1,
+//   url,
+// }))
+
+async function Images() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  console.log(images);
+  return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image) => (
+        <div key={image.id * Math.random()} className="w-48">
+          <img src={image.url} />
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default async function HomePage() {
-
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id)
-  })
-  console.log(images)
-
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-
-        {[...images, ...images,...images].map((image) => (
-          <div key={image.id * Math.random()} className="w-48">
-            <img src={image.url}  />
-          </div>
-        ))}
-      </div>
-
+      <SignedOut>
+        <div className="h-full w-full text-2xl">Please sign in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
