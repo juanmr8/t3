2689fdb603980097862dc736e30215c24dfe3ef4
Juan Mora Romero <info@juanmoraromero.com>
2024-12-04 20:10:58 +0100

    done the custom uploadthing button and added sonner

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,25 +1,22 @@
-"use client";
-import { UploadButton } from "@/utils/uploadthing";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
+import { SimpleUploadButton } from "./simple-upload-button";
 
 export const TopNav = () => {
-  const router = useRouter();
-
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
-      <div className="flex flex-row items-center justify-center">
+      <div className="flex flex-row items-center justify-center gap-4">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <UploadButton
+          <SimpleUploadButton />
+          {/* <UploadButton
             endpoint="imageUploader"
             onClientUploadComplete={() => {
               router.refresh();
             }}
-          />
+          /> */}
           <UserButton />
         </SignedIn>
       </div>
