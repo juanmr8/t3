1e3499e7caea6e3bd7c51907f46e3dee1752cf5b
Juan Mora Romero <info@juanmoraromero.com>
2024-12-09 11:42:15 +0100

    added delete button

---
+++ src/server/analytics.ts
@@ -0,0 +1,15 @@
+import { PostHog } from "posthog-node";
+
+function serverSideAnalytics() {
+  const postHogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    flushAt: 1,
+    flushInterval: 0,
+  });
+
+  return postHogClient;
+}
+
+const analyticsServerClient = serverSideAnalytics();
+
+export default analyticsServerClient;
